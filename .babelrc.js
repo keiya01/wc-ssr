e47c0e858fdd9e4f5414a2a77c886715f9d03dba
keiya01 <keiya.s.0210@gmail.com>
2021-05-08 23:39:55 +0900

    chore: format

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
+      ["@babel/preset-env", { targets: { node: "current" } }],
       "@babel/preset-typescript",
     ],
   };
