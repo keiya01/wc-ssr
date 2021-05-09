7ef746585b04b5ecaa1484ec1f5c75082f9f8311
keiya01 <keiya.s.0210@gmail.com>
2021-05-09 22:02:38 +0900

    chore: format

--- __browserTests__/browser.test.ts
+++ __browserTests__/browser.test.ts
@@ -1,46 +1,59 @@
-import 'expect-puppeteer';
+import "expect-puppeteer";
 
-describe('simple page', () => {
+describe("simple page", () => {
   beforeEach(async () => {
-    await page.goto('http://localhost:3000/simple');
+    await page.goto("http://localhost:3000/simple");
   });
 
-  it('should render simple-page element', async () => {
+  it("should render simple-page element", async () => {
     await page.setJavaScriptEnabled(false);
 
     // for checking SSR
     await page.reload();
 
     const textContent = await page.evaluate(() => {
-      return document.querySelector('simple-page')?.shadowRoot?.querySelector('article > p')?.textContent;
+      return document
+        .querySelector("simple-page")
+        ?.shadowRoot?.querySelector("article > p")?.textContent;
     });
     expect(textContent).toBe("Hello SSR");
 
     await page.setJavaScriptEnabled(true);
   });
 
-  it('should hydrate simple-page element', async () => {
+  it("should hydrate simple-page element", async () => {
     const textContent = await page.evaluate(() => {
-      return document.querySelector('simple-page')?.shadowRoot?.querySelector('article > p')?.textContent;
+      return document
+        .querySelector("simple-page")
+        ?.shadowRoot?.querySelector("article > p")?.textContent;
     });
     expect(textContent).toBe("Hello Client");
   });
 });
 
-describe('passing state as props page', () => {
+describe("passing state as props page", () => {
   beforeEach(async () => {
-    await page.goto('http://localhost:3000/passing-state-as-props');
+    await page.goto("http://localhost:3000/passing-state-as-props");
   });
 
-  it('should be added event listener', async () => {
-    const getButtonTextContent = async () => await page.evaluate(() => {
-      const homePage = document.querySelector('passing-state-as-props-page')?.shadowRoot;
-      return homePage?.querySelector('add-button')?.shadowRoot?.querySelector('button')?.innerText;
-    });
-    const clickButton = async () => await page.evaluate(() => {
-      const homePage = document.querySelector('passing-state-as-props-page')?.shadowRoot;
-      return homePage?.querySelector('add-button')?.shadowRoot?.querySelector('button')?.click();
-    });
+  it("should be added event listener", async () => {
+    const getButtonTextContent = async () =>
+      await page.evaluate(() => {
+        const homePage = document.querySelector("passing-state-as-props-page")
+          ?.shadowRoot;
+        return homePage
+          ?.querySelector("add-button")
+          ?.shadowRoot?.querySelector("button")?.innerText;
+      });
+    const clickButton = async () =>
+      await page.evaluate(() => {
+        const homePage = document.querySelector("passing-state-as-props-page")
+          ?.shadowRoot;
+        return homePage
+          ?.querySelector("add-button")
+          ?.shadowRoot?.querySelector("button")
+          ?.click();
+      });
     expect(await getButtonTextContent()).toBe("hey");
     await clickButton();
     expect(await getButtonTextContent()).toBe("1");
