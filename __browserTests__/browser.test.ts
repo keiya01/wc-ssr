dec00615ddd94e5d3cdac0a7dd74262c80b6f927
keiya01 <keiya.s.0210@gmail.com>
2021-05-26 23:48:48 +0900

    refactor: move passing state page to individual file

--- __browserTests__/browser.test.ts
+++ __browserTests__/browser.test.ts
@@ -30,34 +30,3 @@ describe("simple page", () => {
     expect(textContent).toBe("Hello Client");
   });
 });
-
-describe("passing state as props page", () => {
-  beforeEach(async () => {
-    await page.goto("http://localhost:3000/passing-state-as-props");
-  });
-
-  it("should be added event listener", async () => {
-    const getButtonTextContent = async () =>
-      await page.evaluate(() => {
-        const homePage = document.querySelector("passing-state-as-props-page")
-          ?.shadowRoot;
-        return homePage
-          ?.querySelector("add-button")
-          ?.shadowRoot?.querySelector("button")?.innerText;
-      });
-    const clickButton = async () =>
-      await page.evaluate(() => {
-        const homePage = document.querySelector("passing-state-as-props-page")
-          ?.shadowRoot;
-        return homePage
-          ?.querySelector("add-button")
-          ?.shadowRoot?.querySelector("button")
-          ?.click();
-      });
-    expect(await getButtonTextContent()).toBe("hey");
-    await clickButton();
-    expect(await getButtonTextContent()).toBe("1");
-    await clickButton();
-    expect(await getButtonTextContent()).toBe("2");
-  });
-});
