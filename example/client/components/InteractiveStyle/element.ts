c7c4a1de38dc786c981b4f0ea43ce6d6a5240505
keiya01 <keiya.s.0210@gmail.com>
2021-06-08 22:38:48 +0900

    example: add example about interactive style

---
+++ example/client/components/InteractiveStyle/element.ts
@@ -0,0 +1,23 @@
+import { BaseElement } from "wc-ssr/client";
+import { template, Props } from "./template";
+
+type State = {
+  isOpenDialog: boolean;
+}
+
+export class InteractiveStyle extends BaseElement<Props, State> {
+  state: State = { isOpenDialog: false };
+
+  handleActivateDialog = () => {
+    this.setState({isOpenDialog: !this.state.isOpenDialog });
+  }
+
+  render() {
+    return template({
+      isOpenDialog: this.state.isOpenDialog,
+      handleActivateDialog: this.handleActivateDialog,
+    });
+  }
+}
+
+customElements.define("interactive-style", InteractiveStyle);
