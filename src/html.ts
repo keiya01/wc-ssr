1fbf2494879b85864e121e255a3816e1172e14f6
keiya01 <keiya.s.0210@gmail.com>
2021-06-09 23:38:47 +0900

    fix: fix to use symbol as $$typeof

--- src/html.ts
+++ src/html.ts
@@ -1,7 +1,10 @@
 import { BaseProps, isEvent, isProps, isShadowRoot } from "./attribute";
+import { escapeHTML } from "./escapeHTML";
+import { TEMPLATE_RESULT_TYPE } from "./symbols";
 import { hasProperties } from "./utils";
 
 export type TemplateResult = {
+  $$typeof: symbol;
   strings: TemplateStringsArray;
   values: unknown[];
 };
@@ -10,7 +13,7 @@ export const html = (
   strings: TemplateStringsArray,
   ...values: unknown[]
 ): TemplateResult => {
-  return { strings, values };
+  return { $$typeof: TEMPLATE_RESULT_TYPE, strings, values };
 };
 
 export const ATTRIBUTE_NAME = "private-wc-ssr";
@@ -25,44 +28,52 @@ export const htmlToString = (
 ): string => {
   let windowProps: BaseProps | null = null;
   const htmlString = result.strings.reduce((res, str, i) => {
-    let val = result.values[i];
+    res += str;
+
+    const val = result.values[i];
     if (isTemplateResult(val)) {
-      val = htmlToString(val);
+      return res + htmlToString(val);
     }
     if (isEvent(val)) {
-      val = `${ATTRIBUTE_EVENT_NAME}="true"`;
+      return res + `${ATTRIBUTE_EVENT_NAME}="true"`;
     }
     if (isProps(val)) {
       if (options?.isPage) {
         windowProps = val.props;
-        val =
+        return (
+          res +
           `${ATTRIBUTE_IS_PARENT}="true"` +
           " " +
-          `${ATTRIBUTE_PROPS_NAME}="true"`;
+          `${ATTRIBUTE_PROPS_NAME}="true"`
+        );
       } else {
-        val = `${ATTRIBUTE_PROPS_NAME}="true"`;
+        return res + `${ATTRIBUTE_PROPS_NAME}="true"`;
       }
     }
     if (isShadowRoot(val)) {
-      val = `shadowroot="${val.value}"`;
+      return res + `shadowroot="${val.value}"`;
     }
     if (Array.isArray(val)) {
-      val = val.reduce((res, item) => {
-        if (!item) {
-          return res;
-        }
-        if (isTemplateResult(item)) {
-          res += htmlToString(item);
-          return res;
-        }
-        return res + item;
-      }, "");
+      return (
+        res +
+        val.reduce((res, item) => {
+          if (!item) {
+            return res;
+          }
+          if (isTemplateResult(item)) {
+            res += htmlToString(item);
+            return res;
+          }
+          return res + item;
+        }, "")
+      );
     }
     if (!val) {
-      return res + str;
+      return res;
     }
-    return res + str + val;
+    return res + escapeHTML(String(val));
   }, "");
+
   if (options?.isPage && windowProps) {
     return [
       `<script type="text/javascript">window.__PAGE_ELEMENT_PROPS__=${JSON.stringify(
@@ -75,5 +86,12 @@ export const htmlToString = (
 };
 
 export const isTemplateResult = (obj: unknown): obj is TemplateResult => {
-  return hasProperties<TemplateResult>(obj, ["strings", "values"]);
+  return (
+    hasProperties<TemplateResult>(obj, ["$$typeof", "strings", "values"]) &&
+    obj.$$typeof === TEMPLATE_RESULT_TYPE &&
+    obj.strings &&
+    obj.values &&
+    Array.isArray(obj.strings) &&
+    Array.isArray(obj.values)
+  );
 };
