87b0d987a1323a5108b512d35d0daee400f8160b
keiya01 <keiya.s.0210@gmail.com>
2021-06-19 22:14:05 +0900

    build: fix to use modern build

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = (api) => {
   api.cache(true);
 
-  const presets = [
-    ["@babel/preset-typescript", { parerOpts: { module: "esnext" } }],
-    ["@babel/preset-env", { targets: { chrome: 83 } }],
-  ];
+  const presets = ["@babel/preset-typescript", "@babel/preset-env"];
 
   const plugins = [];
 
